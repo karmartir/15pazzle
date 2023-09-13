7391a3b17612d0b24cef155c45ae32e7ee1b93d9
karmartir <karmartir@gmail.com>
2023-09-12 20:57:11 -0400

    v00

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Board from "./Board";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App: React.FC = () => {
+
+    const initialTiles = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+    /* const initialTiles = new Array(15)
+     for (let i = 0; i < initialTiles.length; i++) {
+         initialTiles[i] = i + 1;
+     }
+ */
+    const [tiles, setTiles] = useState(initialTiles);
+
+    //перемешивание игровых
+    const shuffledTiles = () => {
+        const shuffledTiles = [...initialTiles] // копия массива
+
+        for(let i = shuffledTiles.length -1; i > 0; i--) {
+            const randomNumber = Math.floor(Math.random() * (i + 1));
+
+             [shuffledTiles[i], shuffledTiles[randomNumber]] = [shuffledTiles[randomNumber],shuffledTiles[i]]
+            //деструктурирующая присваивание происходит обмен значениями
+        }
+        setTiles(shuffledTiles)
+    }
+
+
+
+
+    //клик по игровой плитке
+
+    const handleTileClick = (index: number) => {
+        if(canMoveTile(index)) {
+
+            const newTiles = [...tiles]; //копия массива
+            const emptyIndex = newTiles.indexOf(0);
+            [newTiles[index], newTiles[emptyIndex]] = [newTiles[emptyIndex], newTiles[index]];
+            setTiles(newTiles);
+
+            // если игра закончилась, то надпись
+            if(isGameComplete(newTiles)) {
+                alert('Congrats, you won')
+            }
+        }
+    }
+
+    //функция перемещения на пустой (не занятый) индекс
+
+const canMoveTile = (index: number) => {
+    const emptyIndex = tiles.indexOf(0);
+    const row = Math.floor(emptyIndex/4);
+
+    return (
+        (index === emptyIndex - 4 && row > 0) ||
+        (index === emptyIndex -4 && row < 3) ||
+        (index === emptyIndex -1 && index % 4 !== 3) ||
+        (index === emptyIndex + 1 && index % 4 !== 0)
+    )
+
+}
+
+const isGameComplete = (currentTiles: number[]) => {
+        for(let i = 0; i < currentTiles.length -1; i++) {
+            if(currentTiles[i] !== i+1) {
+                return false;
+            }
+        }
+        return true
+}
+
+    return (
+        <div className="App">
+            <h1>15 Puzzle </h1>
+            <Board
+
+            />
+
+            {/*onClick перемешать*/}
+            <button>Shuffle</button>
+        </div>
+    );
 }
 
 export default App;
